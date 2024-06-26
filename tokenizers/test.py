aa661e7a365fb19ca75df8b7732ac97cb39b2da2
Maxime Rochkoulets <maxime.rochkoulets2@mail.dcu.ie>
2024-06-26 11:40:02 +0200

    Removed useless test file.

--- tokenizers/test.py
+++
@@ -1,5 +0,0 @@
-from tokenizers import Tokenizer
-
-tokenizer = Tokenizer.from_file('tokenizers/tokenizer_cfg_500.json')
-print(tokenizer.get_vocab_size())
-print(tokenizer.encode('[0][SEP1][If]').tokens)
